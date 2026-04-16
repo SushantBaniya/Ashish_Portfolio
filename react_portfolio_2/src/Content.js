67f0f58827d354d7336917ba34a9944d4a3e3c75
Sushant Baniya <susantbaniya77@gmail.com>
2026-04-16 09:29:20 +0545

    phonr number change

--- react_portfolio_2/src/Content.js
+++ react_portfolio_2/src/Content.js
@@ -288,9 +288,9 @@ export const content = {
         link: "mailto:aashisbk.av@gmail.com",
       },
       {
-        text: "+977 9709116915",
+        text: "9709116915",
         icon: MdCall,
-        link: "https://wa.me/9779709116915",
+        link: "https://wa.me/9709116915",
       },
       {
         text: "ashish.lamkade",
